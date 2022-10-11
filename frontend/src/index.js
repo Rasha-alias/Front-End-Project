c72803d5de08582d1954e5f24b191f07a99baa37
rasha-alias <rashaziyad000@gmail.com>
2022-10-11 11:38:00 +0200

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+/**
+ * This Component Render The main react component onto the “root” element (which it is marked in the html file). 
+ */
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+/**
+ * @author Rasha Alias <rashaziyad000@gmail.com>
+ */
+
